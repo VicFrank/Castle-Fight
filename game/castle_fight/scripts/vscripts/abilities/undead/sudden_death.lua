85ad6b7a74296da8ff43881f77d9d1fee37a9082
danillissimo <56923895+danillissimo@users.noreply.github.com>
2019-11-08 15:42:31 +0400

    Finished new sudden death particle
    Somewhy wight noise in blood splatter texture becomes clearly visible in castle-fight, no matter what particle is using it. This creates a light effect. Nothing like this is observable in dota map.

--- game/castle_fight/scripts/vscripts/abilities/undead/sudden_death.lua
+++ game/castle_fight/scripts/vscripts/abilities/undead/sudden_death.lua
@@ -15,42 +15,11 @@ function sudden_death:OnSpellStart()
     local particle = ParticleManager:CreateParticle(particleName, PATTACH_ABSORIGIN_FOLLOW, target)
     ParticleManager:ReleaseParticleIndex(particle)
   else
-    -- Should have used bloodrite per-target spilled blood effect probably, and spawned random number
-    -- of them in random places of some area. But this looks nice too.
     target:EmitSound("hero_bloodseeker.bloodRite.silence")
 
-    local impactSource = "particles/units/heroes/hero_axe/axe_culling_blade_kill_warp_b.vpcf"
-    local impact = ParticleManager:CreateParticle(impactSource, PATTACH_ABSORIGIN, target)
-    ParticleManager:SetParticleControl(impact, 4, target:GetAbsOrigin())
-    ParticleManager:ReleaseParticleIndex(impact)
-
-    local splatterSource = "particles/custom/undead/death_pit/blood_splatter.vpcf"
-    local splatter = ParticleManager:CreateParticle(splatterSource, PATTACH_ABSORIGIN, target)
-    ParticleManager:SetParticleControl(splatter, 4, target:GetAbsOrigin())
-    ParticleManager:ReleaseParticleIndex(splatter)
-    for i=1,RandomInt(3, 7) do
-      local bloodWaveSource = "particles/units/heroes/hero_axe/axe_culling_blade_kill_wave_1.vpcf"
-      local bloodWave = ParticleManager:CreateParticle(bloodWaveSource, PATTACH_ABSORIGIN, target)
-      ParticleManager:SetParticleControl(bloodWave, 4, target:GetAbsOrigin())
-      ParticleManager:SetParticleControlForward(bloodWave, 3, RandomVector(1))
-      ParticleManager:ReleaseParticleIndex(bloodWave)
-    end
-    -- for i=1,RandomInt(2, 4) do
-    --   local bloodSource = "particles/units/heroes/hero_axe/axe_culling_blade_pool.vpcf"
-    --   local blood = ParticleManager:CreateParticle(bloodSource, PATTACH_WORLDORIGIN, target)
-    --   ParticleManager:SetParticleControl(blood, 4, target:GetAbsOrigin())
-    --   ParticleManager:SetParticleControlForward(blood, 3, RandomVector(1))
-    --   ParticleManager:ReleaseParticleIndex(blood)
-    -- end
-    -- for i=1,RandomInt(5, 15) do
-    --   local bloodSource = "particles/blood_splatter.vpcf"
-    --   local blood = ParticleManager:CreateParticle(bloodSource, PATTACH_WORLDORIGIN, target)
-    --   ParticleManager:SetParticleControl(blood, 0, target:GetAbsOrigin() + RandomVector(RandomInt(10, 200)))
-    --   -- ParticleManager:SetParticleControlEnt(blood, 1, target, PATTACH_WORLDORIGIN, "", target:GetAbsOrigin() + RandomVector(200), true)
-    --   -- ParticleManager:SetParticleControlEnt(blood, 2, target, PATTACH_WORLDORIGIN, "", target:GetAbsOrigin() + RandomVector(200), true)
-    --   -- ParticleManager:SetParticleControlEnt(blood, 3, target, PATTACH_WORLDORIGIN, "", target:GetAbsOrigin() + RandomVector(200), true)
-    --   ParticleManager:ReleaseParticleIndex(blood)
-    -- end
+    local particle = ParticleManager:CreateParticle("particles/custom/undead/death_pit/sudden_death.vpcf", PATTACH_WORLDORIGIN, target)
+    ParticleManager:SetParticleControl(particle, 4, target:GetAbsOrigin())
+    ParticleManager:ReleaseParticleIndex(particle)
   end
 
   for _,modifier in pairs(target:FindAllModifiers()) do
