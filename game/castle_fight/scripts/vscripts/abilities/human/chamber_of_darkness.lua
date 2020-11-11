8d5c37315b6572cb428920fad87c9f7227f73f78
iSirux <joelssonsimon@gmail.com>
2020-11-11 17:11:37 +0100

    warlock cast blast on target, radius 370->300

--- game/castle_fight/scripts/vscripts/abilities/human/chamber_of_darkness.lua
+++ game/castle_fight/scripts/vscripts/abilities/human/chamber_of_darkness.lua
@@ -4,6 +4,8 @@ function chamber_of_darkness:OnSpellStart()
   -- Ability properties
   local caster = self:GetCaster()
   local ability = self
+  local position = self:GetCursorPosition()
+
   local sound = "Hero_Pugna.NetherBlast"
   local soundPreCast = "Hero_Pugna.NetherBlastPreCast"
   local preCastParticleName = "particles/units/heroes/hero_pugna/pugna_netherblast_pre.vpcf"
@@ -16,19 +18,19 @@ function chamber_of_darkness:OnSpellStart()
   caster:EmitSound(sound)
 
   local particlePre = ParticleManager:CreateParticle(preCastParticleName, PATTACH_CUSTOMORIGIN, nil)
-  ParticleManager:SetParticleControl(particlePre, 0, caster:GetAbsOrigin())
+  ParticleManager:SetParticleControl(particlePre, 0, position)
   ParticleManager:SetParticleControl(particlePre, 1, Vector(radius, delay, 1))
   ParticleManager:ReleaseParticleIndex(particlePre)
 
-  EmitSoundOnLocationForAllies(caster:GetAbsOrigin(), soundPreCast, caster)
+  EmitSoundOnLocationForAllies(position, soundPreCast, caster)
 
   Timers:CreateTimer(delay, function()
     local particleBlast = ParticleManager:CreateParticle(particleName, PATTACH_ABSORIGIN, caster)
-    ParticleManager:SetParticleControl(particleBlast, 0, caster:GetAbsOrigin())
+    ParticleManager:SetParticleControl(particleBlast, 0, position)
     ParticleManager:SetParticleControl(particleBlast, 1, Vector(radius, 0, 0))
     ParticleManager:ReleaseParticleIndex(particleBlast)
 
-    local targets = FindEnemiesInRadius(caster, radius)
+    local targets = FindEnemiesInRadius(caster, radius, position)
 
     for _,unit in pairs(targets) do
       if not IsCustomBuilding(unit) then
