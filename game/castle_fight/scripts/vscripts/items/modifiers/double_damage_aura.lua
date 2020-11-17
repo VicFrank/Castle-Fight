000addbe5f97dfa8c58189dea22cec6c37f35f89
VicFrank <matthewshattil@gmail.com>
2020-11-17 16:28:22 -0600

    updated brazilian translation

--- game/castle_fight/scripts/vscripts/items/modifiers/double_damage_aura.lua
+++ game/castle_fight/scripts/vscripts/items/modifiers/double_damage_aura.lua
@@ -5,7 +5,6 @@ modifier_double_damage_aura = class({})
 function modifier_double_damage_aura:GetTexture() return "item_energy_booster" end
 function modifier_double_damage_aura:IsAura() return true end
 function modifier_double_damage_aura:IsPurgable() return false end
-function modifier_double_damage_aura:GetAuraRadius() return 99999 end
 function modifier_double_damage_aura:GetAuraSearchTeam() return DOTA_UNIT_TARGET_TEAM_FRIENDLY end
 function modifier_double_damage_aura:GetAuraSearchType() return DOTA_UNIT_TARGET_ALL end
 function modifier_double_damage_aura:GetAuraDuration() return 0.5 end
@@ -13,6 +12,27 @@ function modifier_double_damage_aura:GetModifierAura() return "modifier_custom_d
 
 function modifier_double_damage_aura:GetAuraEntityReject(target)
   return IsCustomBuilding(target) or target:IsRealHero()
+end
+
+function modifier_double_damage_aura:OnCreated()
+  if not IsServer() then return end
+  self.creationTime = GameRules:GetGameTime()
+end
+
+function modifier_double_damage_aura:GetAuraRadius()
+  if not IsServer() then return end
+
+  local time = GameRules:GetGameTime() - self.creationTime
+  local radius = 99999
+  local expansionTime = 3
+
+  if time < expansionTime then
+    multiplier = time / expansionTime
+
+    return radius * multiplier
+  end
+
+  return radius
 end
 
 modifier_custom_double_damage = class({})
